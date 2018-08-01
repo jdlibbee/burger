f268d39c3f3d31258f749ece5f3628006e4bcb30
jdlibbee <libbeelibbee@yahoo.com>
2018-07-31 18:18:53 -0600

    updating image

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -5,7 +5,7 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
@@ -15,13 +15,7 @@ if (process.env.JAWSDB_URL) {
 }
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
